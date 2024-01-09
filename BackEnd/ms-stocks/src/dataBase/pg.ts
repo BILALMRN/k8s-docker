63f4de4b8e39bef2a0233e120ed9e58970be2361
BILALMRN <bilalmrn.mail@gmail.com>
2024-01-09 16:54:54 +0100

    feat: add ms stock

---
+++ BackEnd/ms-stocks/src/dataBase/pg.ts
@@ -0,0 +1,55 @@
+import { Pool } from "pg";
+import IDB from "../Interface/IDB";
+import dotenv from "dotenv";
+dotenv.config();
+
+class DB implements IDB {
+  pool: Pool;
+  constructor() {
+    if (!process.env.CONNECTION_STRING_POSTGRES_SQL) {
+      throw new Error("No db environment variable");
+    }
+    const connectionString: string = process.env.CONNECTION_STRING_POSTGRES_SQL;
+
+    this.pool = new Pool({
+      connectionString,
+    });
+  }
+
+  async connect(): Promise<void> {
+    await this.pool.connect();
+    console.log("Connected to the database");
+  }
+
+  async updateStockProduct(product_id: number): Promise<boolean> {
+    try {
+      await this.pool.query(
+        "UPDATE products SET stock = (stock - 1) WHERE product_id = $1 and and stock >= 1",
+        [product_id],
+      );
+      return true;
+    } catch (error) {
+      console.error(error);
+      throw error;
+    }
+  }
+
+
+  async updateStockProductAdmin(product_id: number, quantity: number): Promise<boolean> {
+    try {
+      await this.pool.query(
+        "UPDATE products SET stock = $1 WHERE product_id = $2 and and stock >= 1",
+        [quantity, product_id],
+      );
+      return true;
+    } catch (error) {
+      console.error(error);
+      throw error;
+    }
+  }
+
+}
+
+const DataBase: DB = new DB();
+
+export default DataBase;
