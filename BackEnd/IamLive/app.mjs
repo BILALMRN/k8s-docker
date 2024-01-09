bb77550b0163c04a18141ab4f1c2174b2f77e2b1
BILALMRN <bilalmrn.mail@gmail.com>
2024-01-09 20:26:20 +0100

    Refactor: health check response in app.mjs

--- BackEnd/IamLive/app.mjs
+++ BackEnd/IamLive/app.mjs
@@ -7,10 +7,7 @@ const swaggerDocument = YAML.load('./swagger.yaml')
 const app = express();
 
 app.use(cors());
-
 app.use('/v1', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-app.use('/', (req, res) => {
-    res.send(true); // Send 'true' as the response for the health check
-});
+app.use((req, res) => res.send(true));
 
 app.listen(3000);
