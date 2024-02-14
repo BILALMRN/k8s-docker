2444d7fbb8d75d8c09b9d7f25ee1229df74656a1
BILALMRN <bilalmrn.mail@gmail.com>
2024-02-14 11:16:04 +0100

    update route.ts

--- BackEnd/ms-admins/src/route/route.ts
+++ BackEnd/ms-admins/src/route/route.ts
@@ -4,11 +4,10 @@ import adminController, { validateRequestBody } from "../Controller/adminControl
 
 const itemsRouter = express.Router();
 
+itemsRouter.get("/login", adminController.loginAccountUser);
 // GET /items/:id
 itemsRouter.get("/:id", adminController.getAccount);
-
 // Other routes
-itemsRouter.get("/", adminController.loginAccountUser);
 itemsRouter.post("/", validateRequestBody, adminController.createAccount);
 itemsRouter.put("/", validateRequestBody, adminController.updateAccount);
 itemsRouter.delete("/", adminController.deleteAccount);
